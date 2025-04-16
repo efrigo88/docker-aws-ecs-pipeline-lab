924cd7304639a1f3adfccd2027d566779c1d9da8
Emiliano Frigo <emif.iron@gmail.com>
2025-04-16 12:06:30 +0200

    add logs for lambda

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -2,3 +2,8 @@ resource "aws_cloudwatch_log_group" "etl_task" {
   name              = "/ecs/etl-task"
   retention_in_days = 30
 }
+
+resource "aws_cloudwatch_log_group" "lambda_trigger" {
+  name              = "/aws/lambda/trigger-etl"
+  retention_in_days = 30
+}
